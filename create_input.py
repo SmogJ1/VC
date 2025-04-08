9136ba5c27d8db11bebe3d266b7bce96ebd97a0a
João Miguel <jmfigueiredo03@gmail.com>
2025-04-08 16:32:01 +0100

    start

--- create_input.py
+++ create_input.py
@@ -2,17 +2,11 @@ import os
 import json
 
 image_dir = "images"
-output_json = "data/input.json"
+input_json = "data/input.json"
 
-image_files = [f for f in os.listdir(image_dir) if f.lower().endswith(".jpg")]
+data = {
+    "image_files": [("images/"+f) for f in os.listdir(image_dir) if f.lower().endswith(".jpg")]
+}
 
-input_data = []
-for img in image_files:
-    input_data.append({
-        "image_path": os.path.join(image_dir, img)
-    })
-
-with open(output_json, "w") as f:
-    json.dump(input_data, f, indent=4)
-
-print(f"Input JSON saved to {output_json}")
+with open(input_json, "w") as f:
+    json.dump(data, f, indent=4)
