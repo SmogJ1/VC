4ed5968e45ee19b80612b8c93805133770de51f9
João Miguel <jmfigueiredo03@gmail.com>
2025-04-11 17:27:19 +0100

    stil problems but less

--- pipeline_brute.py
+++ pipeline_brute.py
@@ -49,9 +49,6 @@ for image_num in range(len(image_paths)):
 
     hsv = cv2.cvtColor(masked_image, cv2.COLOR_BGR2HSV)
 
-    lower_brown = np.array([10, 23, 50])
-    upper_brown = np.array([30, 255, 255])
-
     table_color_mask = cv2.inRange(hsv, lower_brown, upper_brown)
     chessboard_mask = cv2.bitwise_not(table_color_mask)
 
@@ -59,7 +56,17 @@ for image_num in range(len(image_paths)):
     chessboard_mask = cv2.morphologyEx(chessboard_mask, cv2.MORPH_CLOSE, kernel)
     chessboard_mask = cv2.morphologyEx(chessboard_mask, cv2.MORPH_OPEN, kernel, iterations=2)
 
-    contours, _ = cv2.findContours(chessboard_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours_, _ = cv2.findContours(chessboard_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    height, width = chessboard_mask.shape[:2]
+
+    def is_touching_border(contour, width, height):
+        for point in contour:
+            x, y = point[0]
+            if x <= 1 or y <= 1 or x >= width - 2 or y >= height - 2:
+                return True
+        return False
+
+    contours = [cnt for cnt in contours_ if not is_touching_border(cnt, width, height)]
 
     # draw contours on the original image
     contour_image = masked_image.copy()
@@ -67,7 +74,13 @@ for image_num in range(len(image_paths)):
         cv2.drawContours(contour_image, [contour], -1, (0, 255, 0), 30)
     contour_rgb = cv2.cvtColor(contour_image, cv2.COLOR_BGR2RGB)
 
-    chessboard_countor = max(contours, key=cv2.contourArea)
+    try :
+        chessboard_countor = max(contours, key=cv2.contourArea)
+    except ValueError:
+        string = f"Error: {image_paths[image_num]} of num {image_num} has no contours"
+        error.append(string)
+        print(string)
+        continue
 
     # draw contours on the original image
     contour_image = masked_image.copy()
@@ -81,7 +94,7 @@ for image_num in range(len(image_paths)):
     masked_image = cv2.bitwise_and(image, chessboard_mask)
     masked_rgb = cv2.cvtColor(masked_image, cv2.COLOR_BGR2RGB)
 
-    approx = cv2.approxPolyDP(chessboard_countor, 0.05 * cv2.arcLength(chessboard_countor, True), True)
+    approx = cv2.approxPolyDP(chessboard_countor, 0.06 * cv2.arcLength(chessboard_countor, True), True)
 
     # draw contours on the original image
     contour_image = masked_image.copy()
@@ -90,8 +103,9 @@ for image_num in range(len(image_paths)):
 
 
     if(len(approx) != 4):
-        error.append(image_paths[image_num])
-        print("Not enough corners found")
+        string = f"Error: {image_paths[image_num]} of num {image_num} has {len(approx)} corners"
+        error.append(string)
+        print(string)
         continue
 
 
